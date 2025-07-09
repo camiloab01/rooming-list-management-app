846b8a004c2b59f61747f167ea80ca5ea4c8837c
Camilo A <camiloab01@gmail.com>
2025-07-09 17:45:11 -0500

    Use cors to allow vite client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,20 @@ import express from 'express'
 import morgan from 'morgan'
 import router from './routes'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+)
 
 app.use('/api', router)
 
