0b23b218412d9efb28ebc09bc03e92cfbdba3a93
Camilo A <camiloab01@gmail.com>
2025-07-09 23:49:05 -0500

    Add show agreement tooltip

--- frontend/src/components/roomingListCard.tsx
+++ frontend/src/components/roomingListCard.tsx
@@ -1,4 +1,7 @@
-import { ArrowRightIcon, CalendarIcon } from '@heroicons/react/16/solid'
+import {
+  DocumentMagnifyingGlassIcon,
+  CalendarDaysIcon,
+} from '@heroicons/react/16/solid'
 import type { RoomingList } from '../types/rooming'
 import DateTag from './dateTag'
 
@@ -28,18 +31,42 @@ export default function RoomingListCard(props: RoomingListCardProps) {
 
       {/* Date range */}
       <div className="flex items-center text-gray-500 text-sm mb-4">
-        <CalendarIcon className="w-4 h-4 mr-1" />
+        <CalendarDaysIcon className="w-4 h-4 mr-1" />
         <span>{rl.date_range}</span>
       </div>
 
       {/* Actions */}
       <div className="flex space-x-2">
-        <button className="flex-1 bg-indigo-600 hover:bg-indigo-700 text-white py-2 rounded-md text-center font-medium">
+        <button className="flex-1 bg-indigo-600 hover:bg-indigo-700 text-white py-2 rounded-md text-center font-medium cursor-pointer">
           View Bookings ({rl.bookingCount})
         </button>
-        <button className="p-2 bg-indigo-50 hover:bg-indigo-100 rounded-md">
-          <ArrowRightIcon className="w-5 h-5 text-indigo-600" />
-        </button>
+        <div className="relative group inline-block">
+          <button className="p-1 border border-indigo-700 rounded-md w-10 h-10 flex items-center justify-center cursor-pointer">
+            <DocumentMagnifyingGlassIcon className="w-5 h-5 text-indigo-700" />
+          </button>
+          <span
+            className="
+              absolute 
+              bottom-full 
+              left-1/2 
+              transform -translate-x-1/2 
+              mb-2 
+              px-2 py-1 
+              text-xs 
+              bg-gray-800 
+              text-white 
+              rounded 
+              opacity-0 
+              group-hover:opacity-100 
+              transition-opacity 
+              pointer-events-none
+              whitespace-nowrap
+              z-10
+            "
+          >
+            Show Agreement as PDF
+          </span>
+        </div>
       </div>
     </div>
   )
